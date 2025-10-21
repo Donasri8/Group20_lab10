7b480bff5bccbb56dfc3ce6240c1cadf946e4a4f
DonasriP <EE25MT015@iitdh.ac.in>
2025-10-21 15:16:23 +0530

    Commiting Lab10- Stack- Overflow checking

--- main.c
+++ main.c
@@ -6,6 +6,12 @@ int series_sum(int k)
     int c=3;
     int d=8;
     int e=5;
+    int i=0;
+    int ar[10000];
+    for (i=0; i<10000;i++)
+    {
+        ar[i]=8;
+    }
     if(k>0)
     {
         return k + series_sum(k-1);
@@ -19,9 +25,9 @@ int series_sum(int k)
 int main(void)
 {
 	int s1,s2,s3;
-	int k=8;
-	int p=10;
-	int m=18;
+	int k=80;
+	int p=1000;
+	int m=180;
 	s1= series_sum(k);
 	s2= series_sum(p);
 	s3= series_sum(m);
